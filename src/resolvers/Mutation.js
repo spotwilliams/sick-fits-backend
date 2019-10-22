4bc8a6782d5814f4caf04586dfc6fd05d933edff
Ricardo Vergara <ricardovergarawork@gmail.com>
2019-10-22 01:01:08 -0300

    Mutation with errorss

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,13 @@
 const Mutations = {
+  async createItem(parent, args, context, info) {
+    console.log(args);
+    // Instead of specificate all fields
+    // we can use this approach
+    let payload = { data: { ...args } };
+    const item = await context.db.mutation.createItem(payload, info);
+
+    return item;
+  },
   createUser(parent, args, context, info) {
     global.users = global.users || [];
     global.users.push(
