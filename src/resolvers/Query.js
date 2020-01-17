dc38856c86678e0f0c38b1c4a8d29dd2a3325a1c
Ricardo Vergara <ricardovergarawork@gmail.com>
2020-01-17 16:02:44 -0300

    Some tweaks, after watch on work

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -11,3 +11,13 @@ const Query = {
   }
 };
 module.exports = Query;
+
+
+/*
+If there is the case that you dont need to perform biz validation, and just use this resolver
+as a proxy, you can use the forwardTo prisma helper.
+1) import ==> const { forwardTo } = require('prisma-binding');
+2) const Query = {
+  items: forwardTo('db')
+}
+* */
