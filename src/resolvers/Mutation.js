1a46f814f4f14a80a4c3312230e8c1b5311d175d
Ricardo Vergara <ricardovergarawork@gmail.com>
2020-02-13 14:50:48 -0300

    Update item

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,24 +2,39 @@
 // and add biz logic and authentication
 
 const Mutations = {
-  async createItem(parent, args, context, info) {
-    // Instead of specificate all fields
-    // we can use this approach
-    let payload = { data: { ...args } };
-    const item = await context.db.mutation.createItem(payload, info);
+    async createItem(parent, args, context, info) {
+        // Instead of specify all fields
+        // we can use this approach
+        let payload = {data: {...args}};
+        const item = await context.db.mutation.createItem(payload, info);
 
-    return item;
-  },
-  createUser(parent, args, context, info) {
-    global.users = global.users || [];
-    global.users.push(
-      (user = {
-        id: Math.random(),
-        name: args.name
-      })
-    );
-    return user;
-  }
+        return item;
+    },
+
+    updateItem(parent, args, context, info) {
+        // Spread the data
+        let updates = {...args};
+        // Remove the ID from updates
+        delete updates.id;
+        return context.db.mutation.updateItem(
+            {
+                data: updates,
+                where: {
+                    id: args.id
+                }
+            }, info);
+    },
+
+    createUser(parent, args, context, info) {
+        global.users = global.users || [];
+        global.users.push(
+            (user = {
+                id: Math.random(),
+                name: args.name
+            })
+        );
+        return user;
+    }
 };
 
 module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,14 +1,17 @@
+const {forwardTo} = require('prisma-binding');
+
 const Query = {
-  users(parent, args, context, info) {
-    global.users = global.users || [];
-    return global.users;
-  },
+    users(parent, args, context, info) {
+        global.users = global.users || [];
+        return global.users;
+    },
 
-  async items(parent, args, context, info) {
-    const items = await context.db.query.items();
+    async items(parent, args, context, info) {
+        const items = await context.db.query.items();
+        return items;
+    },
 
-    return items;
-  }
+    item: forwardTo('db'),
 };
 module.exports = Query;
 
