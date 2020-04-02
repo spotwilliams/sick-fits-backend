17a2b4722234e3f6d8b68909dff328d4c7c17e1b
spotwilliams <ricardovergarawork@gmail.com>
2020-04-02 18:24:14 -0300

    Connections for items

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,26 +1,17 @@
-const {forwardTo} = require('prisma-binding');
+const { forwardTo } = require("prisma-binding");
 
 const Query = {
-    users(parent, args, context, info) {
-        global.users = global.users || [];
-        return global.users;
-    },
+  users(parent, args, context, info) {
+    global.users = global.users || [];
+    return global.users;
+  },
 
-    async items(parent, args, context, info) {
-        const items = await context.db.query.items();
-        return items;
-    },
+  async items(parent, args, context, info) {
+    const items = await context.db.query.items();
+    return items;
+  },
 
-    item: forwardTo('db'),
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db")
 };
 module.exports = Query;
-
-
-/*
-If there is the case that you dont need to perform biz validation, and just use this resolver
-as a proxy, you can use the forwardTo prisma helper.
-1) import ==> const { forwardTo } = require('prisma-binding');
-2) const Query = {
-  items: forwardTo('db')
-}
-* */
