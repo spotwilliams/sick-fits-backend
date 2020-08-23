fdef195f958830c4ad8b60c0366313483cf99f6d
spotwilliams <ricardovergarawork@gmail.com>
2020-08-23 17:10:04 -0300

    SignUp Backend

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 require("dotenv").config({ path: ".env" });
+const cookieParser = require("cookie-parser");
 const createServer = require("./createServer");
 const db = require("./db");
 
 server = createServer();
 
-// TODO: Use express middleware to handle cookies (JWT)
+// Use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
 // TODO: Use express middleware to populate user
 
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
+      origin: process.env.FRONTEND_URL,
+    },
   },
-  deets => {
+  (deets) => {
     console.log(`Server running on port http://localhost:${deets.port}`);
   }
 );
