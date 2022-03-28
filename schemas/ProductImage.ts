0b8309013020ad564dc15b1fc13bbab4ec9ee705
Richard Vergara <ricardo.vergara@adeva.co>
2022-03-28 09:03:26 -0300

    Updated

--- schemas/ProductImage.ts
+++ schemas/ProductImage.ts
@@ -6,7 +6,7 @@ export const cloudinary = {
     cloudName: process.env.CLOUDINARY_CLOUD_NAME,
     apiKey: process.env.CLOUDINARY_KEY,
     apiSecret: process.env.CLOUDINARY_SECRET,
-    folder: 'sickfits'
+    folder: process.env.CLOUDINARY_FOLDER
 };
 
 export const ProductImage = list({
