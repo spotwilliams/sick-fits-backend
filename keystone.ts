911698b2f52b57b3a42347839647f691ff6ef957
Richard Vergara <ricardo.vergara@adeva.co>
2022-03-29 12:55:59 -0300

    Yeah, everybody needs some security

--- keystone.ts
+++ keystone.ts
@@ -55,5 +55,4 @@ export default withAuth(config({
     session: withItemData(statelessSessions(sessionConfig), {
         User: `id name email`
     }),
-    graphql:  { playground: true, apolloConfig: { introspection: true } }
 }));
